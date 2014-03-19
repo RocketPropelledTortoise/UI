a299b1e753f32404074eb1c7c9c85f081a0250a7
Stéphane Goetz <onigoetz@onigoetz.ch>
2014-03-19 23:13:21 +0100

    Composer and Documentation updates

--- README.md
+++ README.md
@@ -7,5 +7,11 @@ Here are all the Components for Rocket Propelled Tortoise CMS - UI
 ### Forms
 Create form fields easily with a clean and fluid syntax
 
+### Foundation
+All views and scripts for the UI Base
+
 ### Script
 Enqueue javascript to put at the end of the page
+
+### Taxonomy
+Taxonomy edition UI
--- subsplit.sh
+++ subsplit.sh
@@ -1,5 +1,7 @@
 #git subsplit available here : https://github.com/dflydev/git-subsplit
 git subsplit init git@github.com:RocketPropelledTortoise/UI.git
 git subsplit publish src/Forms:git@github.com:RocketPropelledTortoise/Forms.git
+git subsplit publish src/Foundation:git@github.com:RocketPropelledTortoise/Foundation.git
 git subsplit publish src/Script:git@github.com:RocketPropelledTortoise/Script.git
+git subsplit publish src/Taxonomy:git@github.com:RocketPropelledTortoise/TaxonomyUI.git
 rm -rf .subsplit/
