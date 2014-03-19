9f464595942ce9989447cbed642de360cdf48198
Stéphane Goetz <onigoetz@onigoetz.ch>
2014-03-19 23:12:17 +0100

    Added Foundation component

---
+++ src/Foundation/ServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Add service providers
+ */
+
+namespace Rocket\UI\Foundation;
+
+/**
+ * Register special service providers
+ */
+class ServiceProvider extends Illuminate\Support\ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
