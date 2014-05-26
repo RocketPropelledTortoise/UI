81d6a18c13503e2b1cc3074ec31de2b1d35cfd58
Stéphane Goetz <onigoetz@onigoetz.ch>
2014-05-26 18:56:10 +0200

    Add assets to subsplit

--- subsplit.sh
+++ subsplit.sh
@@ -1,5 +1,6 @@
 #git subsplit available here : https://github.com/dflydev/git-subsplit
 git subsplit init git@github.com:RocketPropelledTortoise/UI.git
+git subsplit publish src/Assets:git@github.com:RocketPropelledTortoise/Assets.git
 git subsplit publish src/Forms:git@github.com:RocketPropelledTortoise/Forms.git
 git subsplit publish src/Foundation:git@github.com:RocketPropelledTortoise/Foundation.git
 git subsplit publish src/Script:git@github.com:RocketPropelledTortoise/Script.git
